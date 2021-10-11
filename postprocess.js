c067ebf4144f856323ec98b097293d570d76207c
Tse Kit Yam <tsekityam@users.noreply.github.com>
2021-10-11 17:58:33 +0800

    Remove invalid lines from raw files

---
+++ postprocess.js
@@ -0,0 +1,34 @@
+import { CsvFile } from "https://deno.land/x/csv_file/mod.ts";
+import { default as isValid } from "https://deno.land/x/date_fns/isValid/index.js";
+import * as path from "https://deno.land/std/path/mod.ts";
+
+const filename = "raw/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv";
+
+const input = new CsvFile(
+  await Deno.open(filename, {
+    read: true,
+  })
+);
+
+const output = new CsvFile(
+  await Deno.open(path.basename(filename), {
+    write: true,
+    create: true,
+    truncate: true,
+  })
+);
+
+await input.seekRecord(0);
+
+await input.readHeader();
+await output.writeRecord(input.header);
+
+for await (let record of input) {
+  const date = record[0];
+  if (isValid(new Date(date))) {
+    await output.writeRecord(record);
+  }
+}
+
+input.close();
+output.close();
